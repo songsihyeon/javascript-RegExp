3d0fd52cfe99b121f4f43c82c4dc00e5c16b00ac
Songsihyeon <s20040@gsm.hs.kr>
2021-04-20 20:49:08 +0900

    async await

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function sum() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
+async function makeError() {
+  await sleep(1000);
+  const error = new Error();
+  throw error;
+};
+
+async function process() {
+  try {
+    await makeError();
+  } catch (e) {
+    console.error(e);
+  }
 }
-
-sum()
