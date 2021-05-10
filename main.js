f765694e1b5b8102413383608bbd37c8bcbbc182
Songsihyeon <s20040@gsm.hs.kr>
2021-05-10 17:28:03 +0900

    RegExp

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-import _ from 'lodash'
+const str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
 
-console.log('hello world');
-console.log(_.camelCase('hello world'));
+// const regexp = new RegExp('the', 'gi')
+const regexp = /heropy/gi
+
+/* console.log(regexp.test(str)) */
